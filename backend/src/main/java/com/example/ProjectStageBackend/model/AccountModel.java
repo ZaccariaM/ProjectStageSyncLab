133324a086cb65eb9224587dc3ea40fe7f21f31d
ZaccariaM <marangon.zaccaria@gmail.com>
2024-11-12 22:52:46 +0100

    added account controller, model and configured postgreSQL and mongoDB repository

--- backend/src/main/java/com/example/ProjectStageBackend/model/AccountModel.java
+++ backend/src/main/java/com/example/ProjectStageBackend/model/AccountModel.java
@@ -1,4 +1,24 @@
 package com.example.ProjectStageBackend.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data                   //lombok annotations
+@NoArgsConstructor      //lombok annotations
+@Entity                 //postgres annotation
+@Table(name="account")  //postgres annotation
+@Document("Account")    //mongodb annotation
 public class AccountModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String email;
+    private String password;
 }
--- backend/src/main/java/com/example/ProjectStageBackend/repository/AccountRepository.java
+++ backend/src/main/java/com/example/ProjectStageBackend/repository/AccountRepository.java
@@ -1,4 +1,9 @@
 package com.example.ProjectStageBackend.repository;
 
-public interface AccountRepository {
+import com.example.ProjectStageBackend.model.AccountModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+//interface for both mongoDB and postgreSQL repositories
+public interface AccountRepository extends MongoRepository<AccountModel, String>, JpaRepository<AccountModel, String>{
 }
