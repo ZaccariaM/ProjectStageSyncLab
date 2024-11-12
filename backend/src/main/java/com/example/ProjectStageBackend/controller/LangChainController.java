7ddd96e926bc9484e66c4c4403524e3836c699cb
ZaccariaM <marangon.zaccaria@gmail.com>
2024-11-12 22:45:50 +0100

    Added langchain controller and service

--- backend/src/main/java/com/example/ProjectStageBackend/controller/LangChainController.java
+++ backend/src/main/java/com/example/ProjectStageBackend/controller/LangChainController.java
@@ -1,4 +1,22 @@
 package com.example.ProjectStageBackend.controller;
 
+import com.example.ProjectStageBackend.service.LangChainService;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "http:/localhost:4200")
 public class LangChainController {
+    private final LangChainService langChainService;
+
+    public LangChainController(LangChainService langChainService){
+        this.langChainService=langChainService;
+    }
+
+    @GetMapping("/chat")
+    public String chat(@RequestParam(value = "prompt", defaultValue = "Hello")String prompt){
+        return langChainService.chat(prompt);
+    }
 }
--- backend/src/main/java/com/example/ProjectStageBackend/service/LangChainService.java
+++ backend/src/main/java/com/example/ProjectStageBackend/service/LangChainService.java
@@ -1,4 +1,17 @@
 package com.example.ProjectStageBackend.service;
 
+import dev.langchain4j.model.chat.ChatLanguageModel;
+import org.springframework.stereotype.Service;
+
+@Service
 public class LangChainService {
+    private final ChatLanguageModel chatLanguageModel;
+
+    public LangChainService(ChatLanguageModel chatLanguageModel){
+        this.chatLanguageModel=chatLanguageModel;
+    }
+
+    public String chat(String prompt){
+        return chatLanguageModel.generate(prompt);
+    }
 }
